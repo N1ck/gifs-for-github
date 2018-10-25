ff2a55d090bce7cc298e994d99600d741df4ba49
N1ck <nicholas.o.lockhart@gmail.com>
2018-10-26 08:32:36 +1300

    Add masonry, one time binding, element observer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import GphApiClient from 'giphy-js-sdk-core'
+import Masonry from 'masonry-layout'
 import debounce from 'debounce-fn'
 import delegate from 'delegate'
+import gitHubInjection from 'github-injection'
 import { h } from 'dom-chef'
+import observeEl from './simplified-element-observer'
+import onetime from 'onetime'
 import select from 'select-dom'
-
 const client = GphApiClient('Mpy5mv1k9JRY2rt7YBME2eFRGNs7EGvQ')
 
 async function searchGifs (query) {
@@ -14,45 +17,43 @@ async function searchGifs (query) {
 function addButton () {
   for (const toolbar of select.all('form:not(.ghg-has-giphy-field) markdown-toolbar')) {
     const form = toolbar.closest('form')
-    if (!select.exists('.js-manual-file-chooser[type=file]', form)) {
-      continue
-    }
-    const toolbarGroup = select('.toolbar-group:last-child', toolbar)
-    if (toolbarGroup) {
-      toolbarGroup.append(
-        <details class='details-reset details-overlay toolbar-item select-menu select-menu-modal-right ghg-trigger'>
-          <summary class='menu-target' aria-label='Insert a GIF' aria-haspopup='menu'>
-            {'Select a GIF'}
-          </summary>
 
-          <details-menu
-            class='select-menu-modal position-absolute right-0'
-            style={{ 'z-index': 99, width: '480px' }}
-            role='menu'
-          >
-            <div class='select-menu-header d-flex'>
-              <span class='select-menu-title flex-auto'>Select a GIF</span>
-            </div>
-
-            <tab-list>
-              <div class='select-menu-filters'>
-                <div class='select-menu-text-filter'>
-                  <input
-                    type='text'
-                    class='form-control'
-                    placeholder='Search for a GIF…'
-                    aria-label='Search for a GIF'
-                    autofocus=''
-                  />
-                </div>
+    observeEl(toolbar, () => {
+      const toolbarGroup = select('.toolbar-group:last-child', toolbar)
+      if (toolbarGroup) {
+        toolbarGroup.append(
+          <details class='details-reset details-overlay toolbar-item select-menu select-menu-modal-right ghg-trigger'>
+            <summary class='menu-target' aria-label='Insert a GIF' aria-haspopup='menu'>
+              {'Select a GIF'}
+            </summary>
+            <details-menu
+              class='select-menu-modal position-absolute right-0'
+              style={{ 'z-index': 99, width: '480px', height: '410px' }}
+              role='menu'
+            >
+              <div class='select-menu-header d-flex'>
+                <span class='select-menu-title flex-auto'>Select a GIF</span>
               </div>
-              <div id='ghg-giphy-results' />
-            </tab-list>
-          </details-menu>
-        </details>
-      )
-      form.classList.add('ghg-has-giphy-field')
-    }
+              <tab-list>
+                <div class='select-menu-filters'>
+                  <div class='select-menu-text-filter'>
+                    <input
+                      type='text'
+                      class='form-control'
+                      placeholder='Search for a GIF…'
+                      aria-label='Search for a GIF'
+                      autofocus=''
+                    />
+                  </div>
+                </div>
+                <div class='ghg-giphy-results' style={{ height: '500px' }} />
+              </tab-list>
+            </details-menu>
+          </details>
+        )
+        form.classList.add('ghg-has-giphy-field')
+      }
+    })
   }
 }
 
@@ -61,15 +62,32 @@ async function showGiphyPopover (e) {
   const searchQuery = e.target.value
   const parent = e.target.closest('.ghg-has-giphy-field')
   const gifs = await searchGifs(searchQuery)
-  const resultContainer = select('#ghg-giphy-results', parent)
+  const resultsContainer = select('.ghg-giphy-results', parent)
 
-  resultContainer.innerHTML = ''
+  resultsContainer.innerHTML = ''
 
+  const gifsToAdd = []
   gifs.forEach(gif => {
-    const url = gif.images.fixed_height_downsampled.gif_url
+    const url = gif.images.fixed_height_downsampled.url
     const height = Math.floor(gif.images.fixed_width.height * MAX_GIF_WIDTH / gif.images.fixed_width.width)
-    resultContainer.append(<img src={url} height={height} class='ghg-gif-selection' />)
+    const img = (
+      <div style={{ float: 'left', width: '145px' }}><img src={url} height={height} class='ghg-gif-selection' /></div>
+    )
+    gifsToAdd.push(img)
+    resultsContainer.append(img)
   })
+
+  const masonry = new Masonry(
+    resultsContainer,
+    {
+      itemSelector: '.ghg-giphy-results div',
+      columnWidth: 145,
+      gutter: 10,
+      transitionDuration: '0.2s'
+      // fitWidth: true
+    },
+    2000
+  )
 }
 
 function selectGif (e) {
@@ -89,5 +107,12 @@ function listen () {
   delegate('.ghg-has-giphy-field', 'keydown', debounce(showGiphyPopover, { wait: 400 }))
 }
 
-addButton()
-listen()
+// Ensure we only bind events to elements once
+const listenOnce = onetime(listen)
+
+// gitHubInjection fires when there's a pjax:end event
+// on github, this happens when a page is loaded
+gitHubInjection(() => {
+  addButton()
+  listenOnce()
+})
---
+++ simplified-element-observer.js
@@ -0,0 +1,19 @@
+// https://github.com/sindresorhus/refined-github/blob/master/source/libs/simplified-element-observer.js
+export default function (el, listener, options = { childList: true }) {
+  if (typeof el === 'string') {
+    el = document.querySelector(el)
+  }
+
+  if (!el) {
+    return
+  }
+
+  // Run on updates
+  const observer = new MutationObserver(listener)
+  observer.observe(el, options)
+
+  // Run the first time
+  listener.call(observer, [])
+
+  return observer
+}
