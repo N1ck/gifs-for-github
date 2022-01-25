c65799230ce2b41ae556679404bcbf42940866d1
Nick Lockhart <nicholas.o.lockhart@gmail.com>
2022-01-26 10:52:05 +1300

    Attach GIF button to toolbar when editing messages

--- src/main.js
+++ src/main.js
@@ -13,6 +13,8 @@ import LoadingIndicator from './components/loading-indicator.js'
 import GiphyToolbarItem from './components/giphy-toolbar-item.js'
 import Giphy from './lib/giphy.js'
 
+import { onDiffFileLoad, onCommentEdit } from './lib/github-events/on-fragment-load'
+
 // Create a new Giphy Client
 const giphyClient = new Giphy('Mpy5mv1k9JRY2rt7YBME2eFRGNs7EGvQ')
 /**
@@ -359,6 +361,9 @@ gitHubInjection(() => {
   addToolbarButton()
   listenOnce()
   observeDiscussion()
+  onDiffFileLoad(addToolbarButton)
+  onCommentEdit(addToolbarButton)
+
   // Clears all gif search input fields and results.
   // We have to do this because when navigating, github will refuse to
   // load the giphy URLs as it violates their Content Security Policy.
