ef04ba09f7080fb0792450f0e73c9db3170484dd
Nick Lockhart <nicko@canva.com>
2024-01-07 08:10:37 +1300

    Formatting

--- src/main.js
+++ src/main.js
@@ -79,9 +79,7 @@ function addToolbarButton() {
   )) {
     const form = toolbar.closest('form')
 
-    const reviewChangesModal = toolbar.closest(
-      '#review-changes-modal'
-    )
+    const reviewChangesModal = toolbar.closest('#review-changes-modal')
     const reviewChangesList = toolbar.closest(
       '#review-changes-modal .SelectMenu-list'
     )
@@ -93,12 +91,15 @@ function addToolbarButton() {
 
       // The Review changes modal sets an inline width of min(640px, 100vw - 2rem);
       // our button takes up another 32px so we need to adjust the inline style to account for that, otherwise it's hidden.
-      let currentWidth = reviewChangesModal.style.width;
+      const currentWidth = reviewChangesModal.style.width
       if (currentWidth.includes('px')) {
-          // Extracts the value from the string (e.g., 640 from "min(640px, 100vw - 2rem)")
-          let widthValue = parseInt(currentWidth.match(/\d+/)[0]);
-          let modifiedWidth = currentWidth.replace(widthValue + 'px', `${widthValue + 32}px`);
-          reviewChangesModal.style.width = modifiedWidth;
+        // Extracts the value from the string (e.g., 640 from "min(640px, 100vw - 2rem)")
+        const widthValue = Number.parseInt(currentWidth.match(/\d+/)[0], 10)
+        const modifiedWidth = currentWidth.replace(
+          widthValue + 'px',
+          `${widthValue + 32}px`
+        )
+        reviewChangesModal.style.width = modifiedWidth
       }
     }
 
