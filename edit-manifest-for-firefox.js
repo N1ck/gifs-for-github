cf3cfdf1cd682e8d37fc49116f72d001efa5f6a4
Jonghakseo <jonghak.seo@creatrip.com>
2024-10-02 16:16:18 +0900

    feat: add support firefox build

---
+++ edit-manifest-for-firefox.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const manifestPath = path.resolve(__dirname, './distribution/manifest.json');
+
+const isManifestExist = fs.existsSync(manifestPath);
+if (!isManifestExist) {
+  console.error('manifest.json does not exist. Please build first.');
+  process.exit(1);
+}
+
+const manifest = fs.readFileSync(manifestPath, "utf8");
+const manifestObject = JSON.parse(manifest);
+
+const newManifest = {
+  ...manifestObject,
+  background: {
+    scripts: [manifestObject.background.service_worker]
+  }
+}
+delete newManifest.background.service_worker;
+
+fs.writeFileSync(manifestPath, JSON.stringify(newManifest, null, 2));
